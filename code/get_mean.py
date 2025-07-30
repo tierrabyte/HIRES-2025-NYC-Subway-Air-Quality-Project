d0cade81b4772939b209bb33c96fe22f91284e7d
Dorian <dorianguzman966@gmail.com>
2025-07-30 15:32:57 -0400

    (Violin plots for each day (separated)

--- code/get_mean.py
+++ code/get_mean.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+import sub_violin_plots as svp
 from hires_analysis import stations
 
 
@@ -39,30 +39,6 @@ pm2_day_4 = day_4['PM2.5'].dropna()
 pm1_day_4 = day_4['PM1'].dropna()
 co_day_4 = day_4['CO'].dropna()
 time_day_4 = day_4['Time'].dropna()
-
-
-#day 1
-st_125_7_14 = ("13:47:02", "13:59:47")
-st_42_portauthority_7_14 = ("14:21:45" , "14:34:36")
-barclayCenter_7_14 = ("15:09:36", "15:20:06")
-
-#day 2
-st_125_bryantpark_7_15 = ("13:01:17", "13:33:56")
-st_42_bryantpark_7_15 = ("13:49:03", "14:22:04")
-st_74_7_15 = ("14:50:19", "15:23:05")
-courtsquare_7_15 = ("15:31:27", "15:52:04")
-fulton_7_15 = ("16:12:29", "16:22:27")
-
-#day 3
-st_137_7_16 = ("13:32:38", "13:52:25")
-st_225_7_16 = ("14:10:31", "18:20:30")
-st_242_7_16 = ("18:28:43", "18:36:34")
-
-#day 4
-st_72_7_24 = ("14:45:40", "15:10:00")
-st_86_7_24 = ("15:13:42", "15:32:23")
-st_96_7_24 = ("15:36:19", "15:53:12")
-
 
 def find_mean_day_1(street):
     time = time_day_1
@@ -112,33 +88,19 @@ def find_mean_day_4(street):
     
     return mean
 
-# mean_data = {"125th st 7/14": find_mean_day_1(st_125_7_14),
-#              "42nd st Port Authority 7/14": find_mean_day_1(st_42_portauthority_7_14),
-#              "Barclay Center 7/14": find_mean_day_1(barclayCenter_7_14),
-#              "125th st Bryant Park 7/15": find_mean_day_2(st_125_bryantpark_7_15),
-#              "42nd st Bryant Park 7/15": find_mean_day_2(st_42_bryantpark_7_15),
-#              "74th st 7/15": find_mean_day_2(st_74_7_15),
-#              "Court Square 7/15": find_mean_day_2(courtsquare_7_15),
-#              "Fulton 7/15": find_mean_day_2(fulton_7_15),
-#              "137th st 7/16": find_mean_day_3(st_137_7_16),
-#              "225th st 7/16": find_mean_day_3(st_225_7_16),
-#              "242nd st 7/16": find_mean_day_3(st_242_7_16),
-#              "72nd st 7/24": find_mean_day_4(st_72_7_24),
-#              "86th st 7/24": find_mean_day_4(st_86_7_24),
-#              "96th st 7/24": find_mean_day_4(st_96_7_24)}
 
 
-mean_data = {"125th st 7/14": find_mean_day_1(st_125_7_14),
-             "42nd st Port Authority 7/14": find_mean_day_1(st_42_portauthority_7_14),
-             "Barclay Center 7/14": find_mean_day_1(barclayCenter_7_14),
-             "125th st Bryant Park 7/15": find_mean_day_2(st_125_bryantpark_7_15),
-             "42nd st Bryant Park 7/15": find_mean_day_2(st_42_bryantpark_7_15),
-             "74th st 7/15": find_mean_day_2(st_74_7_15),
-             "Court Square 7/15": find_mean_day_2(courtsquare_7_15),
-             "Fulton 7/15": find_mean_day_2(fulton_7_15),
-             "137th st 7/16": find_mean_day_3(st_137_7_16),
-             "225th st 7/16": find_mean_day_3(st_225_7_16),
-             "242nd st 7/16": find_mean_day_3(st_242_7_16),
-             "72nd st 7/24": find_mean_day_4(st_72_7_24),
-             "86th st 7/24": find_mean_day_4(st_86_7_24),
-             "96th st 7/24": find_mean_day_4(st_96_7_24)}
+mean_data = {"125th st 7/14": find_mean_day_1(stations['st_125']),
+             "42nd st Port Authority 7/14": find_mean_day_1(stations['st_42_portauthority']),
+             "Barclay Center 7/14": find_mean_day_1(stations['barclayCenter']),
+             "125th st Bryant Park 7/15": find_mean_day_2(stations['st_125_2']),
+             "42nd st Bryant Park 7/15": find_mean_day_2(stations['st_42_bryantpark']),
+             "74th st 7/15": find_mean_day_2(stations['st_74']),
+             "Court Square 7/15": find_mean_day_2(stations['courtsquare']),
+             "Fulton 7/15": find_mean_day_2(stations['fulton']),
+             "137th st 7/16": find_mean_day_3(stations['st_137']),
+             "225th st 7/16": find_mean_day_3(stations['st_225']),
+             "242nd st 7/16": find_mean_day_3(stations['st_242']),
+             "72nd st 7/24": find_mean_day_4(stations['st_72']),
+             "86th st 7/24": find_mean_day_4(stations['st_86']),
+             "96th st 7/24": find_mean_day_4(stations['st_96'])}
--- code/sub_violin_plots.py
+++ code/sub_violin_plots.py
@@ -1,17 +1,58 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from hires_analysis import stations, violin_plots
 
-data = pd.read_csv("data//1379_2025-7-14.csv")
+data_day_1 = pd.read_csv("C:\\Users\\Dorian\\HIRES-2025-NYC-Subway-Air-Quality-Project\\data\\1379_2025-7-14.csv")
+data_day_2 = pd.read_csv("C:\\Users\\Dorian\\HIRES-2025-NYC-Subway-Air-Quality-Project\\data\\1379_2025-7-15.csv")
+data_day_3 = pd.read_csv("C:\\Users\\Dorian\\HIRES-2025-NYC-Subway-Air-Quality-Project\\data\\1379_2025-7-16.csv")
+data_day_4 = pd.read_csv("C:\\Users\\Dorian\\HIRES-2025-NYC-Subway-Air-Quality-Project\\data\\1379_2025-7-24.csv")
 
-stations = {
+
+day_1 = {
     "st_125" : ("13:47:02", '13:59:47'),
     "st_42_portauthority" : ("14:21:45", "14:34:36"),
     "barclayCenter" : ("15:09:36", "15:20:06")
 }
 
+day_2 = {
+    "st_125_2" : ("13:01:17", "13:33:56" ),
+    "st_42_bryantpark" : ("13:49:03", "14:22:04"),
+    "st_74" : ("14:50:19", "15:23:05"),
+    "courtsquare" : ("15:31:27", "15:52:04"),
+    "fulton" : ("16:12:29", "16:22:27")
+    }
 
+day_3 = {
+    "st_137" : ("13:32:38", "13:52:25"),
+    "st_225" : ("14:10:31", "18:20:30"),
+    "st_242" : ("18:28:43", "18:36:34")
+    }
 
+day_4 = {
+    'st_72' : ("14:45:40", "15:10:00"),
+    'st_86' : ("15:13:42", "15:32:23"),
+    'st_96' : ("15:36:19", "15:53:12")
+    }
+
+all_stations = [
+        day_1["st_125"],
+        day_1["st_42_portauthority"],
+        day_1["barclayCenter"],
+        day_2["st_125_2"],
+        day_2["st_42_bryantpark"],
+        day_2["st_74"],
+        day_2["courtsquare"],
+        day_2["fulton"],
+        day_3["st_137"],
+        day_3["st_225"],
+        day_3["st_242"],
+        day_4["st_72"],
+        day_4["st_86"],
+        day_4["st_96"]
+        ]
+
+days = [day_1, day_2, day_3, day_4]
 
 def get_pm25(data, station):
     pm25 = data['PM2.5'].dropna()
@@ -40,21 +81,24 @@ def get_all_pm25(data, stations):
 
 
 
-all_pm25, all_time = get_all_pm25(data, stations)
-print(all_pm25)
-print(all_time)
+all_pm25_day_1, all_time_day_1 = get_all_pm25(data_day_1, day_1)
+print(all_pm25_day_1)
+print(all_time_day_1)
 
+all_pm25_day_2, all_time_day_2 = get_all_pm25(data_day_2, day_2)
+print(all_pm25_day_2)
+print(all_time_day_2)
 
+all_pm25_day_3, all_time_day_3 = get_all_pm25(data_day_3, day_3)
+print(all_pm25_day_3)
+print(all_time_day_3)
 
-plt.figure(figsize=(10, 6))
-plt.violinplot(all_pm25, showmeans=True, showmedians=True)
-plt.xticks(
-    ticks=range(1, len(stations) + 1),
-    labels=list(stations.keys()),
-    rotation=45
-)
-plt.title("PM2.5 Distribution per Station")
-plt.ylabel("PM2.5 (µg/m³)")
-plt.tight_layout()
-plt.show()
+all_pm25_day_4, all_time_day_4 = get_all_pm25(data_day_4, day_4)
+print(all_pm25_day_4)
+print(all_time_day_4)
 
+all_pm25 = [all_pm25_day_1, all_pm25_day_2, all_pm25_day_3, all_pm25_day_4]
+
+i = 0
+
+violin_plots(all_pm25, i, days)
