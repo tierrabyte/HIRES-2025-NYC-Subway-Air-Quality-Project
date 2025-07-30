15ec5824581c881a66440682fab4aa5260f45eec
eliac6@nycstudents.net <eliac6@nycstudents.net>
2025-07-30 13:46:53 -0400

    update dict

--- code/hires analysis.py
+++ code/hires analysis.py
@@ -1,53 +1,15 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-filename = "C:\\Users\\Angy\\Documents\\iaq_analysis\\hires_project\\1379_2025-7-14.csv"
-
-
-    
-def identify_pollutant():
-    data = pd.read_csv(filename)
-    col = data['PM2.5'].dropna()
-    time = data['Time'].dropna()
-    """
-    plt.plot(time, col, 'r-o', label='PM2.5')
-    plt.xlabel('Time')
-    plt.ylabel('Concentration')
-    plt.show()
-    """
-    
-    #day 1
-    st_125 = ("13:47:02", '13:59:47')
-    st_42_portauthority = ("14:21:45" , "14:34:36")
-    barclayCenter = ("15:09:36", "15:20:06")
-    #day 2
-    st_42_bryantpark  = ()
-    st_74 = ()
-    courtsquare = ()
-    fulton = ()
-    #day 3
-    st_137 = ()
-    st_225 = ()
-    st_242 = ()
-
-
-    
-
-    #print(st_125[0])
-    startIndex =time[time == st_125[0]].index.values[0]
-    endIndex = time[time == st_125[1]].index.values[0]
-
-    subcolumn = col[startIndex: endIndex]
-    subtime = time[startIndex: endIndex]
-
-    print(startIndex)
-    print(endIndex)
-
-    plt.plot(subtime,subcolumn, 'r-o', label = "PM2.5")
-    plt.xticks(fontsize=10, rotation=45)
-    plt.xlabel("Time")
-    plt.ylabel("Concentration")
-    plt.grid(True)
-    plt.show()
-
-identify_pollutant()
+stations = {
+    "st_125" : ("13:47:02", '13:59:47'),
+    "st_42_portauthority" : ("14:21:45", "14:34:36"),
+    "barclayCenter" : ("15:09:36", "15:20:06"),
+    # day 2
+    "st_125_2" : ("13:01:17", "13:33:56" ),
+    "st_42_bryantpark" : ("13:49:03", "14:22:04"),
+    "st_74" : ("14:50:19", "15:23:05"),
+    "courtsquare" : ("15:31:27", "15:52:04"),
+    "fulton" : ("16:12:29", "16:22:27"),
+    # day 3
+    "st_137" : ("13:32:38", "13:52:25"),
+    "st_225" : ("14:10:31", "18:20:30"),
+    "st_242" : ("18:28:43", "18:36:34"),
+}
